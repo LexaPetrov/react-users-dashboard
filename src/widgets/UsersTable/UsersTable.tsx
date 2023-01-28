19a33028e26173835207c2d552d8d9e411d6324b
lexapetrov <al1111997@yandex.ru>
2023-01-29 00:08:01 +0100

    fix: removed label for actions col

--- src/widgets/UsersTable/UsersTable.tsx
+++ src/widgets/UsersTable/UsersTable.tsx
@@ -40,7 +40,7 @@ export const UsersTable: React.FC<UsersTableProps> = memo(() => {
     {
       id: 'actions',
       disablePadding: false,
-      label: 'actions',
+      label: '',
     },
   ];
 
